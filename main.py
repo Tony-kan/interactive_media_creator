909395dc650f9d52122144bd3dc87baa731221ca
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-02-15 21:50:21 +0200

    core : implemented a function to change text  to speech

--- main.py
+++ main.py
@@ -49,3 +49,14 @@ def understand_image(base64_image,api_key):
     def encode_image(image_path):
         with open(image_path,"rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
+        
+
+def text_to_speech(input_text,api_key):
+    inference_params = dict(voice="alloy",speed=1.0,api_key=api_key)
+    model_prediction=Model(
+       "https://clarifai.com/openai/tts/models/openai-tts-1" 
+    ).predict_by_bytes(
+        input_text.encode(),input_type="text",inference_params=inference_params
+    )
+    audio_base64 = model_prediction.outputs[0].data.audio.base64
+    return audio_base64
