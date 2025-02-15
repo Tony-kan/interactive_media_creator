3e1bcecdc88073d51e79eb45d23e24be80c74279
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-02-15 21:38:42 +0200

    core : implemented a function to undestand the image

--- main.py
+++ main.py
@@ -30,3 +30,22 @@ def generate_image(user_description,api_key):
         f.write(output_base64)
 
     return "generated_image.png"
+
+
+def understand_image(base64_image,api_key):
+    prompt = f"Analyze the content of this image and write a creative,
+                engaging story that brings the scene to life.
+                Describe the characters, setting, 
+                and actions in a way that would captivate a young audience:"
+    inferenece_params = dict(temperature=0.2,image_base64=base64_image,api_key=api_key)
+    model_prediction=Model(
+              "https://clarifai.com/openai/chat-completion/models/gpt-4-vision"
+    ).predict_by_bytes(
+        prompt.encode(),input_type="text",inference_params=inferenece_params
+    )  
+    
+    return model_prediction.outputs[0].data.text.raw
+  
+    def encode_image(image_path):
+        with open(image_path,"rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
